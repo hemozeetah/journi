84e71466c9aba80326055982bb77d45c8858fbb8
hemozeetah <ayhamdabah@gmail.com>
2025-06-16 22:10:54 +0300

    fix change image post

--- cmd/api/v1/react/src/components/PostList.jsx
+++ cmd/api/v1/react/src/components/PostList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import "./PostList.css";
 
-export default function PostList({ posts }) {
+function Post({ post }) {
   const navigate = useNavigate();
 
   const handleUserClick = (userID) => {
@@ -17,50 +17,60 @@ export default function PostList({ posts }) {
 
   return (
     <>
-      {posts.map((post) => (
-        <div
-          key={post.id}
-          className="post-card"
-        >
-          <div className="user-container">
-            <img
-              src={"http://localhost:8080" + post.userProfile}
-              alt="User profile"
-              className="user-profile"
-              onClick={() => handleUserClick(post.userID)}
-            />
-            <h2
-              className="user-name"
-              onClick={() => handleUserClick(post.userID)}
-            >
-              {post.userName}
-            </h2>
-          </div>
-          <h3
-            className="place-name"
-            onClick={() => handlePlaceClick(post.placeID)}
+      <div
+        key={post.id}
+        className="post-card"
+      >
+        <div className="user-container">
+          <img
+            src={"http://localhost:8080" + post.userProfile}
+            alt="User profile"
+            className="user-profile"
+            onClick={() => handleUserClick(post.userID)}
+          />
+          <h2
+            className="user-name"
+            onClick={() => handleUserClick(post.userID)}
           >
-            <i>at</i> {post.placeName}
-          </h3>
-          <p className="post-caption">{post.caption}</p>
-          <div className="post-images">
-            <div className="image-slider">
-              <img
-                src={"http://localhost:8080" + post.imagesURL[currentImageIndex]}
-                alt={post.userName}
+            {post.userName}
+          </h2>
+        </div>
+        <h3
+          className="place-name"
+          onClick={() => handlePlaceClick(post.placeID)}
+        >
+          <i>at</i> {post.placeName}
+        </h3>
+        <p className="post-caption">{post.caption}</p>
+        <div className="post-images">
+          <div className="image-slider">
+            <img
+              src={"http://localhost:8080" + post.imagesURL[currentImageIndex]}
+              alt={post.userName}
+            />
+          </div>
+          <div className="image-indicators">
+            {post.imagesURL.map((_, index) => (
+              <div
+                key={index}
+                className={`indicator ${index === currentImageIndex ? 'active' : ''}`}
+                onClick={() => setCurrentImageIndex(index)}
               />
-            </div>
-            <div className="image-indicators">
-              {post.imagesURL.map((_, index) => (
-                <div
-                  key={index}
-                  className={`indicator ${index === currentImageIndex ? 'active' : ''}`}
-                  onClick={() => setCurrentImageIndex(index)}
-                />
-              ))}
-            </div>
+            ))}
           </div>
         </div>
+      </div>
+
+    </>
+  );
+}
+
+export default function PostList({ posts }) {
+
+  return (
+    <>
+      {posts.map((post) => (
+        <Post post={post} />
       ))}
     </>
   );
